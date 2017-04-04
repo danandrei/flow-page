5503265c9caeab25fef18862bd9f74fd31da87b2
Dan Andrei <andreidani96@gmail.com>
2017-04-04 22:13:28 +0300

    Working basic functionality.

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 'use strict';
 
 // Dependencies
-const page = require('page.js');
+const Page = require('page.js');
+const Events = require('events');
 
 module.exports = (config) => {
+    let self = new Events();
 
+    if (!config || !config.routes) {
+        throw new Error('Invalid config object');
+    }
+
+    Page({
+        popstate: true
+    });
+
+    Object.keys(config.routes).forEach(path => {
+        Page(path, (context) => {
+            self.emit('pageChanged', {
+                name: config.routes[path],
+                context: context
+            });
+        });
+    });
+
+    Page('*', ctx => {
+        console.log('not found');
+    });
+
+    if (config.initialDispatch) {
+        Page.redirect(document.location.pathname);
+    }
+
+    // exported methods
+    self.page = page;
+
+    return self;
 };
 
 function page (url) {
-
+    Page(url);
 }
